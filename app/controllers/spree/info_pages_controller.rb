a312d7f159d2f9667b7d6d93d7fe776cf297ced1
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2020-10-03 11:05:07 +0630

    custom page addeok

---
+++ app/controllers/spree/info_pages_controller.rb
@@ -0,0 +1,12 @@
+module Spree
+  class InfoPagesController < Spree::StoreController
+  
+
+    respond_to :html
+
+     def about_us
+  
+  end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Spree::Core::Engine.add_routes do
+    get '/about_us', to: 'info_pages#about_us', as: :about_us
+  end
+
+
 end
